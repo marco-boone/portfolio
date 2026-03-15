b4d26a0cfd6a16d061c7ec3b1714559a48608987
mr-boone <marcoeboone@gmail.com>
2026-03-14 21:08:05 -0500

    link update

--- src/components/index/s_links.jsx
+++ src/components/index/s_links.jsx
@@ -6,7 +6,7 @@ import { GoPackage } from "react-icons/go";
 export default function Links() {
   return (
     <div class="flex gap-6">
-      <a href = "https://github.com/mr-boone" class="shadow-sm shadow-sapphire text-lg p-2 hover:shadow-none hover:scale-[0.95] transition-all duration-100 cursor-pointer rounded-sm">
+      <a href = "https://github.com/marco-boone" class="shadow-sm shadow-sapphire text-lg p-2 hover:shadow-none hover:scale-[0.95] transition-all duration-100 cursor-pointer rounded-sm">
         <FaGithub />
       </a>
       <a href = "https://www.linkedin.com/in/marco-boone-220200262/" class="shadow-sm shadow-yellow text-lg p-2 hover:shadow-none hover:scale-[0.95] transition-all duration-100 cursor-pointer rounded-sm">
